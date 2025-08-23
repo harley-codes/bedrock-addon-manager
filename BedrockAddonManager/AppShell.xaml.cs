61ed63e1e84cc43806bd43a7948233ca5865d0ae
Harley Torrisi <mail@harleycodes.com>
2025-08-23 10:20:45 +1000

    Initial Project Commit

---
+++ BedrockAddonManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BedrockAddonManager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BedrockAddonManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BedrockAddonManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
